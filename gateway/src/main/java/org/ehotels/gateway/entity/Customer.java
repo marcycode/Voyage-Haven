e89521b631ae8a72772ab5ddeb75ec1a4bcff247
thePianoKid <gbraden.dev@gmail.com>
2024-03-30 08:05:22 -0400

    Added auto-generated customer id

--- gateway/src/main/java/org/ehotels/gateway/entity/Customer.java
+++ gateway/src/main/java/org/ehotels/gateway/entity/Customer.java
@@ -1,9 +1,6 @@
 package org.ehotels.gateway.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "customer")
 public class Customer {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "customer_id")
     private Integer customerId;
 
