72c56b462d5580c6a2ad39b2f829890a23ffed83
thePianoKid <gbraden.dev@gmail.com>
2024-03-30 15:24:15 -0400

    Added WebConfig to fix CORS issue

--- gateway/src/main/java/org/ehotels/gateway/entity/HotelChainController.java
+++ gateway/src/main/java/org/ehotels/gateway/entity/HotelChainController.java
@@ -2,6 +2,7 @@ package org.ehotels.gateway.entity;
 
 import org.ehotels.gateway.repositories.HotelChainRepo;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class HotelChainController {
         this.repo = repo;
     }
 
-    @GetMapping("/chains")
+    @GetMapping(value = "/chains", produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<List<HotelChain>> all() {
         return new ResponseEntity<>(repo.findAll(), HttpStatus.OK);
     }
